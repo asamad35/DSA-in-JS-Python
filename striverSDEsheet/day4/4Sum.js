c29b4bc2462a0195ef6d349504ce23ae469188a0
Your Name <you@example.com>
2022-06-02 10:45:44 +0530

    4Sum

--- striverSDEsheet/day4/4Sum.js
+++ striverSDEsheet/day4/4Sum.js
@@ -1,35 +1,44 @@
-var fourSum = function (nums, target) {
-  nums.sort((a, b) => a - b);
-  let size = nums.length;
-  let res = [];
+function fourSum(arr, target) {
+  // sort the array
+  arr.sort((a, b) => a - b);
+  let i, j, left, right;
+  const ans = [];
 
-  if (nums.length === 0) return res;
+  // 1st loop
+  for (i = 0; i < arr.length; i++) {
+    // 2nd loop
+    for (j = i + 1; j < arr.length; j++) {
+      left = j + 1;
+      right = arr.length - 1;
 
-  //   loop1
-  for (let i = 0; i < size; i++) {
-    //   loop2
-    for (let j = i + 1; j < size; j++) {
-      let twoTarget = target - nums[i] - nums[j];
-      let start = j + 1;
-      let end = size - 1;
-      //   loop3
-      while (start < end) {
-        let twoSum = nums[start] + nums[end];
-        if (twoSum < twoTarget) ++start;
-        else if (twoSum > twoTarget) --end;
-        else {
-          let numsStart = nums[start];
-          let numsEnd = nums[end];
-          res.push([nums[i], nums[j], nums[start], nums[end]]);
-          while (start < end && numsStart === nums[start]) ++start;
-          while (start > end && numsEnd === nums[end]) ++end;
+      // 3rd loop till left exceeds right
+      while (left < right) {
+        // getting sum of pair
+        const sum = arr[i] + arr[j] + arr[left] + arr[right];
+
+        // push ans in array if conditions matches
+        if (sum === target) ans.push([arr[i], arr[j], arr[left], arr[right]]);
+
+        // skip the repeating elements from left to avoid duplicacy
+        if (sum < target || sum === target) {
+          while (arr[left] === arr[left + 1]) left++;
+          left++;
+        }
+        // skip the repeating elements from right to avoid duplicacy
+        if (sum > target || sum === target) {
+          while (arr[right] === arr[right - 1]) right--;
+          right--;
         }
       }
-      while (j + 1 < size && nums[j + 1] === nums[j]) ++j;
+      // skip the repeating elements to avoid duplicacy
+      while (arr[j] === arr[j + 1]) j++;
+      // the for loop will do the next j++
     }
-    while (i + 1 < size && nums[i + 1] === nums[i]) ++i;
+    // skip the repeating elements to avoid duplicacy
+    while (arr[i] === arr[i + 1]) i++;
+    // the for loop will do the next j++
   }
-  return res;
-};
+  return ans;
+}
 
 console.log(fourSum([1, 0, -1, 0, -2, 2], 0));

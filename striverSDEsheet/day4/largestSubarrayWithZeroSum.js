e044c5b838470e504807b321902f2a1c909e8a83
Your Name <you@example.com>
2022-06-02 10:52:41 +0530

    largest subarray length with zero sum

--- striverSDEsheet/day4/largestSubarrayWithZeroSum.js
+++ striverSDEsheet/day4/largestSubarrayWithZeroSum.js
@@ -1,13 +1,19 @@
 function maxLen(arr) {
+  //  creating object to store sum: index (at which sum was calculated)
   let obj = {};
-  let n = 0;
+  let maxLength = 0;
   let sum = 0;
 
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
-    if (sum === 0) n = i + 1;
+
+    // return index+1 (length of subarray) if sum === 0
+    if (sum === 0) maxLength = i + 1;
     else {
-      if (obj.hasOwnProperty(sum)) n = Math.max(n, i - obj[sum]);
+      // check if sum exist in obj
+      // if yes, return the length of subarray because it was added to the previous sum that we found in object, but doesnt effect the value of sum. Hence it's summation will be 0.
+      if (sum in obj) n = Math.max(maxLength, i - obj[sum]);
+      // store the sum with index in object for future comparison
       else obj[sum] = i;
     }
   }

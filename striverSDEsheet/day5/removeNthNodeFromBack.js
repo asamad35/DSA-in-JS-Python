0c822f1e46307256d68247fb91de2b3b933f3fdb
Your Name <you@example.com>
2022-06-24 11:56:14 +0530

    deleting nth node from last of linked list optimal solution

--- striverSDEsheet/day5/removeNthNodeFromBack.js
+++ striverSDEsheet/day5/removeNthNodeFromBack.js
@@ -1,5 +1,6 @@
-// Naive approach
+// - Naive approach
 
+/*
 var removeNthFromEnd = function (head, n) {
   // if list is empty
   if (!head.next) return null;
@@ -23,5 +24,39 @@ var removeNthFromEnd = function (head, n) {
     count--;
   }
   tempHead.next = tempHead.next.next;
+  return head;
+};
+*/
+
+// - optimal
+var removeNthFromEnd = function (head, n) {
+  // ! Gist is we have to move slow pointer to (lengthOfList - n) times.
+  //  For this we will first move fastPointers n times. It will cover the n length and //- (lengthOfList - total) will be pending.
+  // So we will simply move fastPointer and slowPointer till fast pointer reaches end. That is we will move (lengthOfList - total) times.
+
+  // if empty return null
+  if (!head.next) return null;
+
+  let fastPointer = new ListNode(0);
+  let slowPointer = new ListNode(0);
+
+  fastPointer.next = head;
+  slowPointer.next = head;
+  fastPointer;
+  while (n) {
+    fastPointer = fastPointer.next;
+    n--;
+  }
+
+  // Handling edge case (if node to be removed is the first node)
+  if (!fastPointer.next) return head.next;
+
+  while (fastPointer.next) {
+    fastPointer = fastPointer.next;
+    slowPointer = slowPointer.next;
+  }
+  // deleting node
+  slowPointer.next = slowPointer.next.next;
+
   return head;
 };

6cbbe9a11477eb05e69465fa56b075387a9d14f3
Your Name <you@example.com>
2022-07-29 17:43:20 +0530

    day 8 subset sum

---
+++ striverSDEsheet/day9/subsets_sums.js
@@ -0,0 +1,32 @@
+/* 
+- we need to get sum of all the possible combinations.
+- It can be achieved if we form a recursion tree.
+
+- At each recursive call we are creating two cases. Either pick or skip that element. 
+- By doing this all the cases will be covered.
+
+*/
+
+function subsetSums(arr, n) {
+  const subsetSumArr = [];
+
+  function getAllSubsetSums(index, sum, arr) {
+    // edge case if we are cross the array boundary push sum.
+    if (index === n) {
+      subsetSumArr.push(sum);
+      return;
+    }
+
+    //   pick the element
+    getAllSubsetSums(index + 1, sum + arr[index], arr);
+
+    //  skip element
+    getAllSubsetSums(index + 1, sum, arr);
+  }
+
+  getAllSubsetSums(0, 0, arr);
+
+  subsetSumArr.sort((a, b) => a - b);
+
+  return subsetSumArr;
+}

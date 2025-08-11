a1f48134b8058d3064798a4252dcc79589b80f8d
asamad35 <samad.abdus3535@gmail.com>
2025-08-11 18:49:40 +0530

    Update

--- DSA-IN-JS/aditya-verma/binarySearch/7findFloorOfElement.js
+++ DSA-IN-JS/aditya-verma/binarySearch/7findFloorOfElement.js
@@ -11,23 +11,25 @@
  *  
  */
 
-function findFloorOfAnElement(arr, searchTerm) {
+
+
+function findFloorOfAnElement(arr, target) {
   let start = 0;
-  let end = arr.length - 1
-  let result = -1
+  let end = arr.length - 1;
+  let res = -1;
+
   while (start <= end) {
-    const mid = Math.floor((start + end) / 2)
-    if (arr[mid] === searchTerm) return mid
-    else  if (arr[mid] < searchTerm) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] <= target) {
+      res = mid
       start = mid + 1
-      result = mid
-    } else if (arr[mid] > searchTerm) {
+    }
+    else {
       end = mid - 1
     }
   }
-
-  return result;
+  return res
 }
 
-
-console.log(findFloorOfAnElement([1, 3, 5, 6], 2))
+console.log(findFloorOfAnElement([1, 3, 5, 6], 10))

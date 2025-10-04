e4185bdf969d9ddd2143f7db63b2f504dbd3c7a7
asamad35 <samad.abdus3535@gmail.com>
2025-10-04 20:59:28 +0530

    Udpate

---
+++ DSA-IN-JS/interview/startingPointOfLinkedListCycle.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+// Watch striver video for better understanding
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let slow = head, fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      slow = head;
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next
+      }
+      return slow
+    }
+  }
+
+  return null
+};
